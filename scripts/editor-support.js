4e55d6b4d2555fce72811dbcc56fa60b310aec74
guptapawan <guptapawan@adobe.com>
2025-08-27 23:25:09 +1000

    dddd

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -93,19 +93,150 @@ async function applyChanges(event) {
   return false;
 }
 
-function attachEventListners(main) {
-  [
-    'aue:content-patch',
-    'aue:content-update',
-    'aue:content-add',
-    'aue:content-move',
-    'aue:content-remove',
-    'aue:content-copy',
-  ].forEach((eventType) => main?.addEventListener(eventType, async (event) => {
-    event.stopPropagation();
-    const applied = await applyChanges(event);
-    if (!applied) window.location.reload();
-  }));
+
+function updateUEInstrumentation() {
+debugger;
+console.log("hello");
+  const main = document.querySelector('main');
+
+  // ----- if browse page, identified by theme
+  if (document.querySelector('body[class^=browse-]')) {
+    // if there is already a editable browse rail on the page
+    const browseRailBlock = main.querySelector('div.browse-rail.block[data-aue-resource]');
+    if (browseRailBlock) {
+      // only more default sections can be added
+      setUEFilter(main, 'main');
+      // no more browse rails can be added
+      setUEFilter(document.querySelector('.section.browse-rail-section'), 'empty');
+    } else {
+      // allow adding default sections and browse rail section
+      setUEFilter(main, 'main-browse');
+    }
+    // Update available blocks for tab sections
+    const tabSections = main.querySelectorAll('div[data-aue-model^="tab-section"]');
+    if (tabSections) {
+      tabSections.forEach((elem) => {
+        setUEFilter(elem, 'tab-section');
+      });
+    }
+
+    // Update available blocks for default sections excluding browse-rail-section and tab-section
+    main.querySelectorAll('.section:not(.browse-rail-section):not([data-aue-model^="tab-section"])').forEach((elem) => {
+      setUEFilter(elem, 'section-browse');
+    });
+
+    return;
+  }
+
+  // ----- if article page, identified by theme
+  if (document.querySelector('body[class^=articles]')) {
+    // update available sections
+    setUEFilter(main, 'main-article');
+    // update available blocks for article content sections
+    const articleContentSection = main.querySelector('.article-content-section');
+    if (articleContentSection) {
+      setUEFilter(articleContentSection, 'article-content-section');
+      setIdsforRTETitles(articleContentSection);
+    }
+    // Update available blocks for tab sections
+    const tabSections = main.querySelectorAll('div[data-aue-model^="tab-section"]');
+    if (tabSections) {
+      tabSections.forEach((elem) => {
+        setUEFilter(elem, 'tab-section');
+      });
+    }
+
+    // Update available blocks for default sections excluding article-header-section, article-content-section and tab-section
+    main
+      .querySelectorAll(
+        '.section:not(.article-content-section):not(.article-header-section):not([data-aue-model^="tab-section"])',
+      )
+      .forEach((elem) => {
+        setUEFilter(elem, 'section-article');
+      });
+
+    return;
+  }
+
+  // ----- if author bio page, identified by theme
+  if (document.querySelector('body[class^=authors-bio-page]')) {
+    // update available sections
+    setUEFilter(main, 'empty');
+    // update the only available default section
+    const section = main.querySelector('.section');
+    // if there is already an author bio block
+    const authorBioBlock = main.querySelector('div.author-bio.block');
+    if (authorBioBlock) {
+      // no more blocks selectable
+      setUEFilter(section, 'empty');
+    } else {
+      // only allow adding author bio blocks
+      setUEFilter(section, 'section-author-bio');
+    }
+  }
+
+  // ----- if header, identified by theme
+  if (document.querySelector('body[class^=header]') || getMetadata('theme') === 'header') {
+    // update available sections
+    setUEFilter(main, 'empty');
+    // update the only available default section
+    const section = main.querySelector('.section');
+    setUEFilter(section, 'section-header');
+  }
+
+  // ----- if footer, identified by theme
+  if (document.querySelector('body[class^=footer]') || getMetadata('theme') === 'footer') {
+    // update available sections
+    setUEFilter(main, 'empty');
+    // update the only available default section
+    const section = main.querySelector('.section');
+    setUEFilter(section, 'section-footer');
+  }
+
+  // ----- if profile pages, identified by theme
+  if (document.querySelector('body[class^=profile]') || getMetadata('theme') === 'profile') {
+    // update available sections
+    setUEFilter(main, 'main-profile');
+  }
+
+  // ----- if signup-flow-modal pages, identified by theme
+  if (document.querySelector('body[class^=signup]')) {
+    // update available sections
+    setUEFilter(main, 'main-signup');
+    main.querySelectorAll('.section').forEach((elem) => {
+      setUEFilter(elem, 'sign-up-flow-section');
+    });
+  }
+
+  // ----- if learning-collections page, identified by theme
+  if (document.querySelector('body[class^=learning-collections]') || getMetadata('theme') === 'learning-collections') {
+    // update available sections
+    setUEFilter(main, 'main-learning-collections');
+  }
+}
+
+function attachEventListeners(main) {
+debugger;
+  ['aue:content-patch', 'aue:content-update', 'aue:content-add', 'aue:content-move', 'aue:content-remove'].forEach(
+    (eventType) =>
+      main?.addEventListener(eventType, async (event) => {
+        event.stopPropagation();
+        const applied = await applyChanges(event);
+        if (applied) {
+          updateUEInstrumentation();
+          renderSEOWarnings();
+          if (main.querySelectorAll('.block.code').length > 0) {
+            const { highlightCodeBlock } = await import('./editor-support-blocks.js');
+            const updatedEl = event.detail?.element ?? main;
+            await highlightCodeBlock(updatedEl);
+          }
+        } else {
+          window.location.reload();
+        }
+      }),
+  );
+
+  main.addEventListener('aue:ui-select', handleEditorSelect);
 }
 
 attachEventListners(document.querySelector('main'));
@@ -117,3 +248,4 @@ decorateRichtext();
 // for new richtext-instrumented elements. this happens for example when using experimentation.
 const observer = new MutationObserver(() => decorateRichtext());
 observer.observe(document, { attributeFilter: ['data-richtext-prop'], subtree: true });
+updateUEInstrumentation();
